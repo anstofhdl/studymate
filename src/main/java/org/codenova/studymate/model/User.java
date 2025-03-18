0d856477b38d55cbf67958e845c18f5b431cafbd
rora <wlstndla563@naver.com>
2025-03-18 11:57:11 +0900

    feat : 모델 및 리포지토리 설정   (회원가입처리용)

---
+++ src/main/java/org/codenova/studymate/model/User.java
@@ -0,0 +1,10 @@
+package org.codenova.studymate.model;
+
+public class User {
+    private String id;
+    private String password;
+    private String name;
+    private int avatarId;
+    private int loginCount;
+    private int studyTime;
+}
